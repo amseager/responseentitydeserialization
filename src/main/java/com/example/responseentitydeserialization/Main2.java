bb177743d89a740bacf6d33284a9e47aa5ac5946
amseager <amseager@mail.ru>
2019-11-21 21:51:06 +0300

    Initial commit

---
+++ src/main/java/com/example/responseentitydeserialization/Main2.java
@@ -0,0 +1,33 @@
+package com.example.responseentitydeserialization;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Main2 {
+
+	public static void main(String[] args) throws Exception {
+
+		ResponseEntity<Object> build = ResponseEntity.ok().build();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.addMixIn(ResponseEntity.class, ResponseEntityMixin.class);
+
+		String s = objectMapper.writeValueAsString(build);
+
+		ResponseEntity<Object> result = objectMapper.readValue(s, ResponseEntity.class);
+
+		System.out.println(result);
+
+
+	}
+
+	public abstract static class ResponseEntityMixin<T> {
+
+		@JsonCreator
+		public ResponseEntityMixin(@JsonProperty("body") T body, @JsonProperty("status") HttpStatus status) {
+		}
+	}
+}
